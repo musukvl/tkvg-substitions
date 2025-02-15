98db0ecade1079fc6223c40ac72cb9ef25cda372
Alexey Ryazhskikh <musukvl@gmail.com>
2025-02-15 21:54:10 +0200

    Add command line parameters

--- src/TkvgSubstitutionBot/TkvgCli/Program.cs
+++ src/TkvgSubstitutionBot/TkvgCli/Program.cs
@@ -1,18 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using McMaster.Extensions.CommandLineUtils;
 using TkvgSubstitution;
 
-var tkvgService = new TkvgSubstitutionService();
-var nextDate = Utils.GetNextWorkingDay(DateTime.Now);
-var nextDateString = nextDate.Date.ToString("yyyy-MM-dd");
-Console.WriteLine("Date: " + nextDateString);
-var substitutions = await tkvgService.GetSubstitutions(nextDateString);
-foreach (var sub in substitutions)
+public class Program
 {
-    Console.WriteLine(sub.ClassName);
-    foreach (var s in sub.Substitutions)
+    [Option(Description = "Class", ShortName = "c", LongName = "class")]
+    public string? Class { get; } 
+    
+    [Option(Description = "Date", ShortName = "d", LongName = "date")]
+    public string? Date { get; }
+
+    [Option(Description = "Verbose", ShortName = "v", LongName = "verbose")]
+    public bool Verbose { get; } = false;
+    
+    public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+    private async Task<int> OnExecute()
     {
-        Console.WriteLine("    " + s.Period + " " + s.Info);
+        var tkvgService = new TkvgSubstitutionService();
+
+        var nextDateString = Date;
+        if (string.IsNullOrEmpty(nextDateString))
+        {
+            var date = Utils.GetNextWorkingDay(DateTime.Now);
+            nextDateString = date.Date.ToString("yyyy-MM-dd");
+        }
+
+        if (Verbose)
+            Console.WriteLine("Date: " + nextDateString);
+        if (Class != null)
+            if (Verbose)
+                Console.WriteLine("Class: " + Class);
+        
+        var substitutions = await tkvgService.GetSubstitutions(nextDateString);
+        foreach (var sub in substitutions)
+        {
+            if (Class != null && !string.Equals(sub.ClassName, Class, StringComparison.CurrentCultureIgnoreCase))
+                continue;
+            foreach (var s in sub.Substitutions)
+            {
+                Console.WriteLine("    " + s.Period + " " + s.Info);
+            }
+        }
+
+        return 0;
     }
 }
 
