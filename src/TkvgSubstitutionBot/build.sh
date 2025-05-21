63c057749530269e7cd48335f3d88432a037895a
Alexey Ryazhskikh <musukvl@gmail.com>
2025-05-21 21:31:36 +0300

    push version to 1.0.4

--- src/TkvgSubstitutionBot/build.sh
+++ src/TkvgSubstitutionBot/build.sh
@@ -1,2 +1,2 @@
-docker build -t musukvl/tkvg-substitution-bot:1.0.1 -f TkvgSubstitutionBot/Dockerfile . 
-docker push musukvl/tkvg-substitution-bot:1.0.1
+docker build -t musukvl/tkvg-substitution-bot:1.0.4 -f TkvgSubstitutionBot/Dockerfile . 
+docker push musukvl/tkvg-substitution-bot:1.0.4
